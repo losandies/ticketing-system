aee9da0b85acd67b6c545fd3c8d60f111fef1f57
Brandon Newsome <brandon.i.newsome@gmail.com>
2022-06-06 12:15:50 -0400

    initializing heroku

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ var distDir = __dirname + '/dist/';
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 
-	app.use(express.static(distDir));
+	app.use(express.static('../client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
